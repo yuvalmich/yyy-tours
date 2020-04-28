24a833bb1aaae6650d243eb0dd316ee763b5cffa
Daniel Joseph <jdanieljo@gmail.com>
2020-04-28 17:10:12 +0300

    add basic models

---
+++ Models/Trip.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace yyy_tours.Models
+{
+    public class Trip
+    {
+
+        public string Id { get; set; }
+        public string DisplayName { get; set; }
+        public string PlaceId { get; set; }
+        public string GuideEmail { get; set; }
+        public int Price { get; set; }
+        public int Capacity { get; set; }
+
+
+        public Trip()
+        {
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace yyy_tours.Models
+{
+    public class User
+    {
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public UserType Type { get; set; }
+
+        public User()
+        {
+        }
+    }
+
+    public enum UserType
+    {
+        Tourist, Guide, Admin
+    }
+}
