2b45db08c1bcceedd185d8774701dce845936ab7
Yuval <yuvalmich2@gmail.com>
2020-04-29 16:33:22 +0300

    add validation

--- Models/User.cs
+++ Models/User.cs
@@ -7,16 +7,19 @@ namespace yyy_tours.Models
 
     public class User
     {
-        [Required, EmailAddress]
+        [Required(ErrorMessage = "מייל אינו יכול להיות ריק")]
+        [EmailAddress(ErrorMessage = "מייל אינו תקין")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "שם אינו יכול להיות ריק")]
         public string FullName { get; set; }
 
-        [Required, Phone]
+        [Required(ErrorMessage = "מספר טלפון אינו יכול להיות ריק")]
+        [Phone(ErrorMessage = "מספר טלפון אינו תקין")]
         public string Phone { get; set; }
 
-        [RegularExpression(@"^([a-zA-Z0-9@#$%^&+=*]{6,10})$")]
+        [Required(ErrorMessage = "סיסמה אינה יכולה להיות ריקה")]
+        [RegularExpression(@"^([a-zA-Z0-9@#$%^&+=*]{6,10})$", ErrorMessage ="סיסמה לא תקינה")]
         public string Password { get; set; }
 
         public UserType Type { get; set; }
