e09a4fbaac5db1af294d2cf79b694a919f050aef
Yuval <yuvalmich2@gmail.com>
2020-04-29 14:01:38 +0300

    user model validation

--- Models/User.cs
+++ Models/User.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 namespace yyy_tours.Models
 {
+
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     public class User
     {
+        [Required, EmailAddress]
         public string Email { get; set; }
+
+        [Required]
         public string FullName { get; set; }
+
+        [Required, Phone]
         public string Phone { get; set; }
+
+        [RegularExpression(@"^([a-zA-Z0-9@#$%^&+=*]{6,10})$")]
         public string Password { get; set; }
+
         public UserType Type { get; set; }
 
-        public User()
-        {
-        }
+        
+        public User() {}
     }
 
     public enum UserType
