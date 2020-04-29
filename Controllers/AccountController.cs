e09c420f7e0c55f7f68e11df91def5a40c061b2d
Yuval <yuvalmich2@gmail.com>
2020-04-29 15:13:42 +0300

    refactor names and start using validation for sign up

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using yyy_tours.Models;
+
+namespace yyy_tours.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public IActionResult SignUp()
+        {
+            return View("SignUp");
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToPage("Index");
+            }
+
+            return View("SignUp", model);
+        }
+    }
+}
