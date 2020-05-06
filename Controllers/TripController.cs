5c6e5c3b3541a3caba950466ac4c20aa50cd137d
Daniel Joseph <jdanieljo@gmail.com>
2020-05-06 19:59:30 +0300

    connect models

--- Controllers/TripController.cs
+++ Controllers/TripController.cs
@@ -18,7 +18,19 @@ namespace yyy_tours.Controllers
                 DisplayName = "טיול של בוקר למצדה",
                 Description = "בתחילת הטיפוס שלכם לאורך שביל הנחש (זמן טיפוס משוער כ45 דקות )– השמש תתחיל לזרוח ותוכלו להנות מהנוף המרהיב. אחרי שתגיעו לפסגה תוכלו לחקור את האתר הארכיאולוגי שנבנה על ידי המלך הרודוס",
                 PlaceId = "mesada",
+                Place = new Place()
+                {
+                    Id = "mesada",
+                    Name = "מצדה",
+                    Description = "מבצר עתיק על פסגתו של צוק מבודד, בשוליו המזרחיים של מדבר יהודה, המתנאשא לגובה של 63 מטרים מעל פני הים, וכ-450 מטרים מעל ים המלח שלמרגלותיו",
+                    ImageUrl = "https://img.oastatic.com/img2/15281261/max/besucher-in-masada.jpg",
+                    GoogleMapEmbededUrl = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13634.96476502167!2d35.371785313395826!3d31.31089758714479!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x150305b8cab1b3e1%3A0x3850c0aaa6a68544!2z157XpteT15Q!5e0!3m2!1siw!2sil!4v1588782761948!5m2!1siw!2sil"
+                },
                 GuideEmail = "aa@yyy.com",
+                Guide = new User(){
+                    FullName = "יוסי פורטוגזי",
+                    Type = UserType.Guide
+                },
                 Price = 100,
                 Capacity = 20,
                 Date = new DateTime(2020,5,14,4,30,0), // 14/5/20 04:30
--- Models/Place.cs
+++ Models/Place.cs
@@ -7,7 +7,8 @@ namespace yyy_tours.Models
         public string Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string ImageUrl { get; set; }    
+        public string ImageUrl { get; set; } 
+        public string GoogleMapEmbededUrl { get; set; }   
 
         public Place()
         {
