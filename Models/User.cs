29cf67ea50fb7f7144c4ccb288c3386842119178
Yuval <yuvalmich2@gmail.com>
2020-08-26 15:21:17 +0300

    delete user

--- Models/User.cs
+++ Models/User.cs
@@ -6,21 +6,26 @@ namespace yyytours.Models
     public class User
     {  
         [Key]
+        [Display(Name = "אימייל")]
         [Required(ErrorMessage = "מייל אינו יכול להיות ריק")]
         [EmailAddress(ErrorMessage = "מייל אינו תקין")]
         public string Email { get; set; }
 
+        [Display(Name = "שם מלא")]
         [Required(ErrorMessage = "שם אינו יכול להיות ריק")]
         public string FullName { get; set; }
 
+        [Display(Name = "טלפון")]
         [Required(ErrorMessage = "מספר טלפון אינו יכול להיות ריק")]
         [Phone(ErrorMessage = "מספר טלפון אינו תקין")]
         public string Phone { get; set; }
 
+        [Display(Name = "סיסמה")]
         [Required(ErrorMessage = "סיסמה אינה יכולה להיות ריקה")]
         [RegularExpression(@"^([a-zA-Z0-9@#$%^&+=*]{6,10})$", ErrorMessage = "סיסמה לא תקינה")]
         public string Password { get; set; }
 
+        [Display(Name = "תפקיד")]
         public UserType Type { get; set; }
 
         public User() { }
