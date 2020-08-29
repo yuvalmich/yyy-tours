2b36ba2ca86d95772fbe472ef6b6a99b8c5e36cb
Daniel Joseph <jdanieljo@gmail.com>
2020-08-29 17:06:27 +0300

    set place pages

--- Models/Place.cs
+++ Models/Place.cs
@@ -7,9 +7,13 @@ namespace yyytours.Models
     {
         [Key]
         public string ID { get; set; }
+        [Display(Name = "שם")]
         public string Name { get; set; }
+        [Display(Name = "תיאור")]
         public string Description { get; set; }
+        [Display(Name = "כתובת לתמונה")]
         public string ImageUrl { get; set; } 
+        [Display(Name = "מדינה")]
         public string Country { get; set; }   
 
         public Place()
