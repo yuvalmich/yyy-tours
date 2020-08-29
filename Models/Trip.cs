95ad75b97c3c5dee20b85ead36877bcc91c67fac
Daniel Joseph <jdanieljo@gmail.com>
2020-08-29 17:06:36 +0300

    fix trip pages

--- Models/Trip.cs
+++ Models/Trip.cs
@@ -10,26 +10,39 @@ namespace yyytours.Models
         [Key]
         public string ID { get; set; }
         [Display(Name = "שם")]
+        [Required(ErrorMessage = "שם אינו יכול להיות ריק")]
         public string DisplayName { get; set; }
+        
         [Display(Name = "תיאור")]
+        [Required(ErrorMessage = "תיאור אינו יכול להיות ריק")]
         public string Description { get; set; }
         [Display(Name = "מיקום")]
+        [Required(ErrorMessage = "מיקום אינו יכול להיות ריק")]
         [ForeignKey("Place")]
         public string PlaceId { get; set; }
         [Display(Name = "מיקום")]
+        [Required(ErrorMessage = "מיקום אינו יכול להיות ריק")]
         public Place Place { get; set; }
         [ForeignKey("Guide")]
         [Display(Name = "מדריך")]
+        [Required(ErrorMessage = "מדריך אינו יכול להיות ריק")]
         public string GuideId { get; set; }
         [Display(Name = "מדריך")]
+        [Required(ErrorMessage = "מדריך אינו יכול להיות ריק")]
         public User Guide { get; set; }
         [Display(Name = "מחיר")]
+        [Required(ErrorMessage = "מחיר אינו יכול להיות ריק")]
+        [Range(0, double.MaxValue)]
         public int Price { get; set; }
         [Display(Name = "תפוסה")]
+        [Required(ErrorMessage = "תפוסה אינו יכול להיות ריק")]
         public int Capacity { get; set; }
         [Display(Name = "תאריך ושעה")]
+        [Required(ErrorMessage = "תאריך ושעת הטיול אינו יכול להיות ריק")]
         public DateTime Date { get; set; }
         [Display(Name = "זמן הטיול בשעות")]
+        [Required(ErrorMessage = "זמן הטיול בשעות אינו יכול להיות ריק")]
+        [Range(0.5, 24)]
         public int TimeInHours { get; set; }
 
 
