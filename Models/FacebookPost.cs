1075a1ac3829f9a41d7160b34f35f890b22862b4
Yuval <yuvalmich2@gmail.com>
2020-08-30 00:18:51 +0300

    add facebook post model

---
+++ Models/FacebookPost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace yyytours.Models
+{
+    public class FacebookPost
+    {
+
+        public string created_time;
+        public string message;
+        public string id;
+
+    }
+}
