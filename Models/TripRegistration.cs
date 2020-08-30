822073ecd52c3d84ef040916eb2aa124df621234
Yuval Assa <assa.yuval107@gmail.com>
2020-08-30 17:47:09 +0300

    add trip registration model

---
+++ Models/TripRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using yyytours.Controllers;
+
+namespace yyytours.Models
+{
+    public class TripRegistration
+    {
+        [Key]
+        public string ID { get; set; }
+        [ForeignKey("Trip")]
+        public string TripId { get; set; }
+        [ForeignKey("User")]
+        public string UserEmail { get; set; }
+        public DateTime RegistrationDateTime { get; set; }
+
+        
+        public TripRegistration() { }
+
+
+    }
+}
