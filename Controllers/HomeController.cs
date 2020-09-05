98a70d13b379c220a557f7a67a2695011ea07b7b
Daniel Joseph <jdanieljo@gmail.com>
2020-09-05 14:40:13 +0300

    add recomended places

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,22 +6,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using yyytours.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace yyytours.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly yyyWebProjContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, yyyWebProjContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public async Task<IActionResult> Index()
         {
+
             var posts = await FacebookApi.GetPagePosts();
-            return View(posts);
+            var recommenedePlaces = CreatePlacesStatistics();
+            return View(new HomeViewModel {FacebookPosts = posts, RecomendedPlaces = recommenedePlaces});
         }
 
         public IActionResult Privacy()
@@ -33,6 +38,24 @@ namespace yyytours.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel {  });
+        }
+
+        private IEnumerable<Tuple<Place, int>> CreatePlacesStatistics()
+        {
+            var recommenedeTripsIds = from i in _context.TripRegistration
+                orderby i.RegistrationDateTime descending
+                group i by i.TripId into tripRegs
+                orderby tripRegs.Count() descending
+                select tripRegs.Key;
+             var recommenedeTripsIdsLst = recommenedeTripsIds.ToList();
+             var recommenedePlaces = _context.Trip.Include(t => t.Place)
+             .Where(i=>recommenedeTripsIds.Contains(i.ID)).ToList()
+             .OrderBy(i=> recommenedeTripsIdsLst.IndexOf(i.ID))
+             .GroupBy(i=>i.Place)
+             .OrderByDescending(byPlace => byPlace.Count())
+             .Select(byPlace => new Tuple<Place, int>(byPlace.Key, byPlace.Where(i=>i.Date >= DateTime.Now).Count()));
+
+            return recommenedePlaces;
         }
     }
 }
