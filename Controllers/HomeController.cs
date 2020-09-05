0142828f19a6936951a5e9b994b9364c07cd3cd8
Daniel Joseph <jdanieljo@gmail.com>
2020-09-05 11:33:53 +0300

    set error page view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,7 +32,7 @@ namespace yyytours.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel {  });
         }
     }
 }
--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -4,8 +4,12 @@ namespace yyytours.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public string ErrorDescription { get; set; }
+        public string ControllerToLink { get; set; }
+        public string ActionToLink { get; set; }
+        public string TextToLink { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowErrorDescription => !string.IsNullOrEmpty(ErrorDescription);
+        public bool ShowLink => !string.IsNullOrEmpty(TextToLink) && !string.IsNullOrEmpty(ActionToLink) && !string.IsNullOrEmpty(ControllerToLink);
     }
 }
