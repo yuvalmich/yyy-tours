fcee05cdeaa70415dcb0c81b59bb9b51767351e7
Yuval Assa <assa.yuval107@gmail.com>
2020-09-05 21:03:25 +0300

    add manager graph function

--- Controllers/TripRegistrationController.cs
+++ Controllers/TripRegistrationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+//using System.Data.Entity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -62,6 +63,39 @@ namespace yyy_tours
 
             var registeredUsersByTrips = (await _context.TripRegistration.Include(tr => tr.Trip).Include(tr => tr.User).OrderBy(tr => tr.RegistrationDateTime).ToListAsync()).GroupBy(tr => tr.Trip.DisplayName);
             return View("../TripRegistration/TripManager", registeredUsersByTrips);
+        }
+
+        public async Task<IActionResult> ManagerGraphs()
+        {
+            if (HttpContext.Session.GetString("Email") == null || HttpContext.Session.GetString("Email") == "" || getSessionUserType() != UserType.Admin)
+            {
+                return View("../User/NotAuthorized");
+            }
+
+            //var registeredUsersByTrips = (await _context.TripRegistration.Include(tr => tr.Trip).Include(tr => tr.User).OrderBy(tr => tr.RegistrationDateTime).ToListAsync()).GroupBy(tr => tr.Trip.DisplayName);
+
+
+            ViewData["Graph1"] = (await _context.TripRegistration.Include(tr => tr.Trip).ToListAsync()).OrderBy(tr => tr.RegistrationDateTime).GroupBy(tr => tr.RegistrationDateTime.Date).Select(tr => new RegistrationByDateCount
+            {
+                date = tr.Key.Date.ToShortDateString(),
+                registeredCount = tr.Count()
+            }).ToList();
+
+
+            var registeredUsersCount = (await _context.TripRegistration.Include(tr => tr.Trip).ToListAsync()).GroupBy(tr => tr.Trip.DisplayName).Select(tr => new TripsRegistrationCount
+            {
+                tripName = tr.Key,
+                registeredCount = tr.Count()
+            }).ToList();
+
+            Dictionary<string, int> registeredUsersCountDict = new Dictionary<string, int>();
+            foreach (var item in registeredUsersCount)
+            {
+                registeredUsersCountDict.Add(item.tripName, item.registeredCount);
+            }
+            ViewData["Graph2"] = registeredUsersCountDict;
+
+            return View("../TripRegistration/ManagerGraph");
         }
 
         // GET: TripRegistration/Details/5
@@ -215,5 +249,17 @@ namespace yyy_tours
         {
             return _context.TripRegistration.Any(e => e.ID == id);
         }
+    }
+
+    public class TripsRegistrationCount
+    {
+        public string tripName { get; set; }
+        public int registeredCount { get; set; }
+    }
+
+    public class RegistrationByDateCount
+    {
+        public string date { get; set; }
+        public int registeredCount { get; set; }
     }
 }
