8c68a7d794ad83cb4bb10e0d2bcd6a233926b41e
Yuval <yuvalmich2@gmail.com>
2020-09-07 21:20:46 +0300

    remove not uses pages and functionality

--- Controllers/TripRegistrationController.cs
+++ Controllers/TripRegistrationController.cs
@@ -72,8 +72,6 @@ namespace yyy_tours
                 return View("../User/NotAuthorized");
             }
 
-            //var registeredUsersByTrips = (await _context.TripRegistration.Include(tr => tr.Trip).Include(tr => tr.User).OrderBy(tr => tr.RegistrationDateTime).ToListAsync()).GroupBy(tr => tr.Trip.DisplayName);
-
 
             ViewData["Graph1"] = (await _context.TripRegistration.Include(tr => tr.Trip).ToListAsync()).OrderBy(tr => tr.RegistrationDateTime).GroupBy(tr => tr.RegistrationDateTime.Date).Select(tr => new RegistrationByDateCount
             {
@@ -98,102 +96,7 @@ namespace yyy_tours
             return View("../TripRegistration/ManagerGraph");
         }
 
-        // GET: TripRegistration/Details/5
-        public async Task<IActionResult> Details(string id)
-        {
-            if (HttpContext.Session.GetString("Email") == "")
-            {
-                return View("../User/NotAuthorized");
-            }
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var tripRegistration = await _context.TripRegistration
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (tripRegistration == null)
-            {
-                return NotFound();
-            }
-
-            return View(tripRegistration);
-        }
-
-        // GET: TripRegistration/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: TripRegistration/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,TripId,UserEmail,RegistrationDateTime")] TripRegistration tripRegistration)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(tripRegistration);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(tripRegistration);
-        }
-
-        // GET: TripRegistration/Edit/5
-        public async Task<IActionResult> Edit(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var tripRegistration = await _context.TripRegistration.FindAsync(id);
-            if (tripRegistration == null)
-            {
-                return NotFound();
-            }
-            return View(tripRegistration);
-        }
-
-        // POST: TripRegistration/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("ID,TripId,UserEmail,RegistrationDateTime")] TripRegistration tripRegistration)
-        {
-            if (id != tripRegistration.ID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(tripRegistration);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TripRegistrationExists(tripRegistration.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(tripRegistration);
-        }
-
-        // GET: TripRegistration/Delete/5
         public async Task<IActionResult> Delete(string id)
         {
             if (HttpContext.Session.GetString("Email") == "")
@@ -234,7 +137,6 @@ namespace yyy_tours
             return View("../TripRegistration/TripManager", registeredUsersByTrips);
         }
 
-        // POST: TripRegistration/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(string id)
